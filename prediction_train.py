6541ae493c0d1a23be43eb4fa676a9fe3fbed5b7
Xiaochuan Chen <xca64@lib7000-rcmac04.lib.sfu.ca>
2017-03-30 18:19:45 -0700

    Refactore prediction net

--- prediction_train.py
+++ prediction_train.py
@@ -24,7 +24,8 @@ from tensorflow.python.platform import app
 from tensorflow.python.platform import flags
 
 from data.prediction_input import build_tfrecord_input
-from model.prediction_model import construct_model
+
+from model.prediction_net import Model
 
 # How often to record tensorboard summaries.
 SUMMARY_INTERVAL = 40
@@ -77,143 +78,13 @@ flags.DEFINE_float('learning_rate', 0.001,
 
 
 
-## Helper functions
-def peak_signal_to_noise_ratio(true, pred):
-  """Image quality metric based on maximal signal power vs. power of the noise.
-
-  Args:
-    true: the ground truth image.
-    pred: the predicted image.
-  Returns:
-    peak signal to noise ratio (PSNR)
-  """
-  return 10.0 * tf.log(1.0 / mean_squared_error(true, pred)) / tf.log(10.0)
-
-
-def mean_squared_error(true, pred):
-  """L2 distance between tensors true and pred.
-
-  Args:
-    true: the ground truth image.
-    pred: the predicted image.
-  Returns:
-    mean squared error between ground truth and predicted image.
-  """
-  return tf.reduce_sum(tf.square(true - pred)) / tf.to_float(tf.size(pred))
 
 import moviepy.editor as mpy
 def npy_to_gif(npy, filename):
      clip = mpy.ImageSequenceClip(list(npy), fps=10)
      clip.write_gif(filename)
 
-class Model(object):
 
-  def __init__(self,
-               images=None,
-               actions=None,
-               states=None,
-               sequence_length=None,
-               reuse_scope=None):
-
-    if sequence_length is None:
-      sequence_length = FLAGS.sequence_length
-
-    self.prefix = prefix = tf.placeholder(tf.string, [])
-    self.iter_num = tf.placeholder(tf.float32, [])
-    summaries = []
-
-    # Split into timesteps.
-    actions = tf.split(actions, actions.get_shape().as_list()[1],1)
-    actions = [tf.squeeze(act) for act in actions]          
-    
-    states = tf.split( states, states.get_shape().as_list()[1],1)
-    states = [tf.squeeze(st) for st in states]
-
-    images = tf.split(images, images.get_shape().as_list()[1],1 )
-    images = [tf.squeeze(img) for img in images]
-  
-    if reuse_scope is None:
-      gen_images, gen_states = construct_model(
-          images,
-          actions,
-          states,
-          iter_num=self.iter_num,
-          k=FLAGS.schedsamp_k,
-          use_state=FLAGS.use_state,
-          num_masks=FLAGS.num_masks,
-          cdna=FLAGS.model == 'CDNA',
-          dna=FLAGS.model == 'DNA',
-          stp=FLAGS.model == 'STP',
-          context_frames=FLAGS.context_frames)
-    else:  # If it's a validation or test model.
-      with tf.variable_scope(reuse_scope, reuse=True):
-        gen_images, gen_states = construct_model(
-            images,
-            actions,
-            states,
-            iter_num=self.iter_num,
-            k=FLAGS.schedsamp_k,
-            use_state=FLAGS.use_state,
-            num_masks=FLAGS.num_masks,
-            cdna=FLAGS.model == 'CDNA',
-            dna=FLAGS.model == 'DNA',
-            stp=FLAGS.model == 'STP',
-            context_frames=FLAGS.context_frames)
-
-    # L2 loss, PSNR for eval.
-    loss, psnr_all = 0.0, 0.0
-
-    try:
-       tf.equal(prefix,'train').eval()
-    except:
-       prefixs='initial'
-    else:
-       if tf.equal(prefix,'train').eval():
-           prefixs='train'
-       else:
-           prefixs='val'
-
-    print('Prefix is', prefixs)
-    for i, x, gx in zip(
-        range(len(gen_images)), images[FLAGS.context_frames:],
-        gen_images[FLAGS.context_frames - 1:]):
-      recon_cost = mean_squared_error(x, gx)
-      psnr_i = peak_signal_to_noise_ratio(x, gx)
-      psnr_all += psnr_i
-
-      summaries.append(
-          tf.summary.scalar(prefixs + '_recon_cost' + str(i), recon_cost))
-
-      summaries.append(tf.summary.scalar(prefixs + '_psnr' + str(i), psnr_i))
-      
-      loss += recon_cost
-
-    for i, state, gen_state in zip(
-        range(len(gen_states)), states[FLAGS.context_frames:],
-        gen_states[FLAGS.context_frames - 1:]):
-      state_cost = mean_squared_error(state, gen_state) * 1e-4
-      summaries.append(
-          tf.summary.scalar(prefixs + '_state_cost' + str(i), state_cost))
-      loss += state_cost
-    summaries.append(tf.summary.scalar(prefixs + '_psnr_all', psnr_all))
-    self.psnr_all = psnr_all
-
-    self.loss = loss = loss / np.float32(len(images) - FLAGS.context_frames)
-
-    summaries.append(tf.summary.scalar(prefixs + '_loss', loss))
-
-    # Add image to summary
-
-    summaries.append(tf.summary.image(prefixs + '_gen', gen_images[0], max_outputs=3))    
-    summaries.append(tf.summary.image(prefixs + '_org', images[0], max_outputs=3))
-
-    self.lr = tf.placeholder_with_default(FLAGS.learning_rate, ())
-
-    self.train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
-    self.summ_op = tf.summary.merge(summaries)
-
-    self.gen_images = gen_images
-    self.images = images
 
 def main(unused_argv):
   tf.logging.set_verbosity(tf.logging.INFO)
