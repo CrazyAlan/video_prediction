a0f786123c7ba57f82a35eeacae9b12b002efa5c
CrazyAlan <xiaochuan.chen91@gmail.com>
2017-04-11 17:52:12 -0700

    Refactorized spatial stream

--- model/spatial_stream.py
+++ model/spatial_stream.py
@@ -2,119 +2,181 @@ import numpy as np
 from datetime import datetime
 import tensorflow as tf
 import os
-from network.prediction_net import construct_model
 from tensorflow.python.platform import app
 from tensorflow.python.platform import flags
 
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.contrib.slim.nets import resnet_v1
+slim = tf.contrib.slim
 
 FLAGS = flags.FLAGS
+
+def _add_loss_summaries(total_loss):
+    """Add summaries for losses.
+  
+    Generates moving average for all losses and associated summaries for
+    visualizing the performance of the network.
+  
+    Args:
+      total_loss: Total loss from loss().
+    Returns:
+      loss_averages_op: op for generating moving averages of losses.
+    """
+    # Compute the moving average of all individual losses and the total loss.
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+#     losses = tf.get_collection('losses')
+    loss_averages_op = loss_averages.apply([total_loss])
+  
+    # Attach a scalar summmary to all individual losses and the total loss; do the
+    # same for the averaged version of the losses.
+    for l in [total_loss]:
+        # Name each loss as '(raw)' and name the moving average version of the loss
+        # as the original loss name.
+        tf.summary.scalar(l.op.name +'raw', l)
+        tf.summary.scalar(l.op.name, loss_averages.average(l))
+  
+    return loss_averages_op
+
+def train(total_loss, global_step, optimizer, learning_rate, moving_average_decay, update_gradient_vars, log_histograms=True):
+    # Generate moving averages of all losses and associated summaries.
+    loss_averages_op = _add_loss_summaries(total_loss)
+
+    # Compute gradients.
+    with tf.control_dependencies([loss_averages_op]):
+        if optimizer=='ADAGRAD':
+            opt = tf.train.AdagradOptimizer(learning_rate)
+        elif optimizer=='ADADELTA':
+            opt = tf.train.AdadeltaOptimizer(learning_rate, rho=0.9, epsilon=1e-6)
+        elif optimizer=='ADAM':
+            opt = tf.train.AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=0.1)
+        elif optimizer=='RMSPROP':
+            opt = tf.train.RMSPropOptimizer(learning_rate, decay=0.9, momentum=0.9, epsilon=1.0)
+        elif optimizer=='MOM':
+            opt = tf.train.MomentumOptimizer(learning_rate, 0.9, use_nesterov=True)
+        else:
+            raise ValueError('Invalid optimization algorithm')
+    
+        grads = opt.compute_gradients(total_loss, update_gradient_vars)
+        
+    # Apply gradients.
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+  
+    # Add histograms for trainable variables.
+    if log_histograms:
+        for var in tf.trainable_variables():
+            tf.summary.histogram(var.op.name, var)
+   
+    # Add histograms for gradients.
+    if log_histograms:
+        for grad, var in grads:
+            if grad is not None:
+                tf.summary.histogram(var.op.name + '/gradients', grad)
+  
+    # Track the moving averages of all trainable variables.
+    variable_averages = tf.train.ExponentialMovingAverage(
+        moving_average_decay, global_step)
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+  
+    with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
+        train_op = tf.no_op(name='train')
+  
+    return train_op
+
+def _get_variables_to_train():
+  """Returns a list of variables to train.
+
+  Returns:
+    A list of variables to train by the optimizer.
+  """
+  if FLAGS.trainable_scopes is None:
+    return tf.trainable_variables()
+  else:
+    scopes = [scope.strip() for scope in FLAGS.trainable_scopes.split(',')]
+
+  variables_to_train = []
+  for scope in scopes:
+    variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+    variables_to_train.extend(variables)
+  return variables_to_train
+
+def _get_init_fn():
+  """Returns a function run by the chief worker to warm-start the training.
+
+  Note that the init_fn is only run when initializing the model during the very
+  first global step.
+
+  Returns:
+    An init function run by the supervisor.
+  """
+  if FLAGS.checkpoint_path is None:
+    return None
+
+  # Warn the user if a checkpoint exists in the train_dir. Then we'll be
+#   # ignoring the checkpoint anyway.
+#   if tf.train.latest_checkpoint(FLAGS.train_dir):
+#     tf.logging.info(
+#         'Ignoring --checkpoint_path because a checkpoint already exists in %s'
+#         % FLAGS.train_dir)
+#     return None
+
+  exclusions = []
+  if FLAGS.checkpoint_exclude_scopes:
+    exclusions = [scope.strip()
+                  for scope in FLAGS.checkpoint_exclude_scopes.split(',')]
+
+  # TODO(sguada) variables.filter_variables()
+  variables_to_restore = []
+  for var in slim.get_model_variables():
+    excluded = False
+    for exclusion in exclusions:
+      if var.op.name.startswith(exclusion):
+        excluded = True
+        break
+    if not excluded:
+      variables_to_restore.append(var)
+
+  if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
+    checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
+  else:
+    checkpoint_path = FLAGS.checkpoint_path
+
+  tf.logging.info('Fine-tuning from %s' % checkpoint_path)
+
+  return slim.assign_from_checkpoint_fn(
+      checkpoint_path,
+      variables_to_restore,
+      ignore_missing_vars=FLAGS.ignore_missing_vars)
 
 
 class Model(object):
 
   def __init__(self,
-               images=None,
-               actions=None,
-               states=None,
-               sequence_length=None,
+               images,
+               labels,
+               global_step,
                reuse_scope=None):
 
-    if sequence_length is None:
-      sequence_length = FLAGS.sequence_length
+    labels = slim.one_hot_encoding(
+      tf.string_to_number(labels, out_type=tf.int32), FLAGS.nrof_classes)
+    # inputs has shape [batch, 224, 224, 3]
+    with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+      net, end_points = resnet_v1.resnet_v1_50(images, FLAGS.nrof_classes)
+    cross_entropy = tf.losses.softmax_cross_entropy(
+        labels,tf.squeeze(net))
 
-    self.prefix = prefix = tf.placeholder(tf.string, [])
-    self.iter_num = tf.placeholder(tf.float32, [])
-    summaries = []
+    train_op = train(cross_entropy, global_step,
+                     FLAGS.optimizer, FLAGS.learning_rate, 
+                     0.9999, _get_variables_to_train()) 
 
-    # Split into timesteps.
-    actions = tf.split(actions, actions.get_shape().as_list()[1],1)
-    actions = [tf.squeeze(act) for act in actions]          
-    
-    states = tf.split( states, states.get_shape().as_list()[1],1)
-    states = [tf.squeeze(st) for st in states]
+    correct_prediction = tf.equal(tf.argmax(tf.squeeze(end_points['predictions']),1), tf.argmax(labels,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    images = tf.split(images, images.get_shape().as_list()[1],1 )
-    images = [tf.squeeze(img) for img in images]
-  
-    if reuse_scope is None:
-      gen_images, gen_states = construct_model(
-          images,
-          actions,
-          states,
-          iter_num=self.iter_num,
-          k=FLAGS.schedsamp_k,
-          use_state=FLAGS.use_state,
-          num_masks=FLAGS.num_masks,
-          cdna=FLAGS.model == 'CDNA',
-          dna=FLAGS.model == 'DNA',
-          stp=FLAGS.model == 'STP',
-          context_frames=FLAGS.context_frames)
-    else:  # If it's a validation or test model.
-      with tf.variable_scope(reuse_scope, reuse=True):
-        gen_images, gen_states = construct_model(
-            images,
-            actions,
-            states,
-            iter_num=self.iter_num,
-            k=FLAGS.schedsamp_k,
-            use_state=FLAGS.use_state,
-            num_masks=FLAGS.num_masks,
-            cdna=FLAGS.model == 'CDNA',
-            dna=FLAGS.model == 'DNA',
-            stp=FLAGS.model == 'STP',
-            context_frames=FLAGS.context_frames)
+    init_from_checkpoint = _get_init_fn()
 
-    # L2 loss, PSNR for eval.
-    loss, psnr_all = 0.0, 0.0
+    summary_op = tf.summary.merge_all()
 
-    try:
-       tf.equal(prefix,'train').eval()
-    except:
-       prefixs='initial'
-    else:
-       if tf.equal(prefix,'train').eval():
-           prefixs='train'
-       else:
-           prefixs='val'
-
-    print('Prefix is', prefixs)
-    for i, x, gx in zip(
-        range(len(gen_images)), images[FLAGS.context_frames:],
-        gen_images[FLAGS.context_frames - 1:]):
-      recon_cost = mean_squared_error(x, gx)
-      psnr_i = peak_signal_to_noise_ratio(x, gx)
-      psnr_all += psnr_i
-
-      summaries.append(
-          tf.summary.scalar(prefixs + '_recon_cost' + str(i), recon_cost))
-
-      summaries.append(tf.summary.scalar(prefixs + '_psnr' + str(i), psnr_i))
-      
-      loss += recon_cost
-
-    for i, state, gen_state in zip(
-        range(len(gen_states)), states[FLAGS.context_frames:],
-        gen_states[FLAGS.context_frames - 1:]):
-      state_cost = mean_squared_error(state, gen_state) * 1e-4
-      summaries.append(
-          tf.summary.scalar(prefixs + '_state_cost' + str(i), state_cost))
-      loss += state_cost
-    summaries.append(tf.summary.scalar(prefixs + '_psnr_all', psnr_all))
-    self.psnr_all = psnr_all
-
-    self.loss = loss = loss / np.float32(len(images) - FLAGS.context_frames)
-
-    summaries.append(tf.summary.scalar(prefixs + '_loss', loss))
-
-    # Add image to summary
-
-    summaries.append(tf.summary.image(prefixs + '_gen', gen_images[0], max_outputs=3))    
-    summaries.append(tf.summary.image(prefixs + '_org', images[0], max_outputs=3))
-
-    self.lr = tf.placeholder_with_default(FLAGS.learning_rate, ())
-
-    self.train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
-    self.summ_op = tf.summary.merge(summaries)
-
-    self.gen_images = gen_images
-    self.images = images
+    self.init_from_checkpoint = init_from_checkpoint
+    self.accuracy = accuracy
+    self.cross_entropy = cross_entropy
+    self.train_op = train_op
+    self.summary_op = summary_op
--- recognition_train.py
+++ recognition_train.py
@@ -91,142 +91,7 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.contrib.slim.nets import resnet_v1
 slim = tf.contrib.slim
 
-def _add_loss_summaries(total_loss):
-    """Add summaries for losses.
-  
-    Generates moving average for all losses and associated summaries for
-    visualizing the performance of the network.
-  
-    Args:
-      total_loss: Total loss from loss().
-    Returns:
-      loss_averages_op: op for generating moving averages of losses.
-    """
-    # Compute the moving average of all individual losses and the total loss.
-    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
-#     losses = tf.get_collection('losses')
-    loss_averages_op = loss_averages.apply([total_loss])
-  
-    # Attach a scalar summmary to all individual losses and the total loss; do the
-    # same for the averaged version of the losses.
-    for l in [total_loss]:
-        # Name each loss as '(raw)' and name the moving average version of the loss
-        # as the original loss name.
-        tf.summary.scalar(l.op.name +'raw', l)
-        tf.summary.scalar(l.op.name, loss_averages.average(l))
-  
-    return loss_averages_op
-
-def train(total_loss, global_step, optimizer, learning_rate, moving_average_decay, update_gradient_vars, log_histograms=True):
-    # Generate moving averages of all losses and associated summaries.
-    loss_averages_op = _add_loss_summaries(total_loss)
-
-    # Compute gradients.
-    with tf.control_dependencies([loss_averages_op]):
-        if optimizer=='ADAGRAD':
-            opt = tf.train.AdagradOptimizer(learning_rate)
-        elif optimizer=='ADADELTA':
-            opt = tf.train.AdadeltaOptimizer(learning_rate, rho=0.9, epsilon=1e-6)
-        elif optimizer=='ADAM':
-            opt = tf.train.AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=0.1)
-        elif optimizer=='RMSPROP':
-            opt = tf.train.RMSPropOptimizer(learning_rate, decay=0.9, momentum=0.9, epsilon=1.0)
-        elif optimizer=='MOM':
-            opt = tf.train.MomentumOptimizer(learning_rate, 0.9, use_nesterov=True)
-        else:
-            raise ValueError('Invalid optimization algorithm')
-    
-        grads = opt.compute_gradients(total_loss, update_gradient_vars)
-        
-    # Apply gradients.
-    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
-  
-    # Add histograms for trainable variables.
-    if log_histograms:
-        for var in tf.trainable_variables():
-            tf.summary.histogram(var.op.name, var)
-   
-    # Add histograms for gradients.
-    if log_histograms:
-        for grad, var in grads:
-            if grad is not None:
-                tf.summary.histogram(var.op.name + '/gradients', grad)
-  
-    # Track the moving averages of all trainable variables.
-    variable_averages = tf.train.ExponentialMovingAverage(
-        moving_average_decay, global_step)
-    variables_averages_op = variable_averages.apply(tf.trainable_variables())
-  
-    with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
-        train_op = tf.no_op(name='train')
-  
-    return train_op
-
-def _get_variables_to_train():
-  """Returns a list of variables to train.
-
-  Returns:
-    A list of variables to train by the optimizer.
-  """
-  if FLAGS.trainable_scopes is None:
-    return tf.trainable_variables()
-  else:
-    scopes = [scope.strip() for scope in FLAGS.trainable_scopes.split(',')]
-
-  variables_to_train = []
-  for scope in scopes:
-    variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
-    variables_to_train.extend(variables)
-  return variables_to_train
-
-def _get_init_fn():
-  """Returns a function run by the chief worker to warm-start the training.
-
-  Note that the init_fn is only run when initializing the model during the very
-  first global step.
-
-  Returns:
-    An init function run by the supervisor.
-  """
-  if FLAGS.checkpoint_path is None:
-    return None
-
-  # Warn the user if a checkpoint exists in the train_dir. Then we'll be
-#   # ignoring the checkpoint anyway.
-#   if tf.train.latest_checkpoint(FLAGS.train_dir):
-#     tf.logging.info(
-#         'Ignoring --checkpoint_path because a checkpoint already exists in %s'
-#         % FLAGS.train_dir)
-#     return None
-
-  exclusions = []
-  if FLAGS.checkpoint_exclude_scopes:
-    exclusions = [scope.strip()
-                  for scope in FLAGS.checkpoint_exclude_scopes.split(',')]
-
-  # TODO(sguada) variables.filter_variables()
-  variables_to_restore = []
-  for var in slim.get_model_variables():
-    excluded = False
-    for exclusion in exclusions:
-      if var.op.name.startswith(exclusion):
-        excluded = True
-        break
-    if not excluded:
-      variables_to_restore.append(var)
-
-  if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
-    checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
-  else:
-    checkpoint_path = FLAGS.checkpoint_path
-
-  tf.logging.info('Fine-tuning from %s' % checkpoint_path)
-
-  return slim.assign_from_checkpoint_fn(
-      checkpoint_path,
-      variables_to_restore,
-      ignore_missing_vars=FLAGS.ignore_missing_vars)
-
+from model.spatial_stream import Model
 
 def main(unused_argv):
   tf.logging.set_verbosity(tf.logging.INFO)
@@ -234,28 +99,14 @@ def main(unused_argv):
   with tf.Graph().as_default():
     print('Constructing models and inputs.')
     images, labels = build_tfrecord_input()
-    labels = slim.one_hot_encoding(
-      tf.string_to_number(labels, out_type=tf.int32), FLAGS.nrof_classes)
+   
     global_step = tf.Variable(0, trainable=False)
 
-    # inputs has shape [batch, 224, 224, 3]
-    with slim.arg_scope(resnet_v1.resnet_arg_scope()):
-      net, end_points = resnet_v1.resnet_v1_50(images, FLAGS.nrof_classes)
-    cross_entropy = tf.losses.softmax_cross_entropy(
-        labels,tf.squeeze(net))
-
-    train_op = train(cross_entropy, global_step,
-                     FLAGS.optimizer, FLAGS.learning_rate, 
-                     0.9999, _get_variables_to_train()) 
+    model = Model(images, labels, global_step)
 
     # import pdb
     # pdb.set_trace()
-    correct_prediction = tf.equal(tf.argmax(tf.squeeze(end_points['predictions']),1), tf.argmax(labels,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    init_from_checkpoint = _get_init_fn()
-
-    summary_op = tf.summary.merge_all()
+   
     
     print('Constructing saver.')
     # Make saver.
@@ -285,14 +136,14 @@ def main(unused_argv):
     # pdb.set_trace()
     # Init from checkpoint
     if FLAGS.checkpoint_path is not None:
-      init_from_checkpoint(sess)
+      model.init_from_checkpoint(sess)
 
     print('Start Tranning')
     # Run training.
     for itr in range(FLAGS.num_iterations):
       # import pdb
       # pdb.set_trace()
-      cost, _, summary_str, acc = sess.run([cross_entropy, train_op, summary_op, accuracy])
+      cost, _, summary_str, acc = sess.run([model.cross_entropy, model.train_op, model.summary_op, model.accuracy])
       tf.logging.info('  In Iteration ' + str(itr) + ', Cost ' + str(cost) + ', Accuracy ' + str(acc))
 
       if (itr) % VAL_INTERVAL == 20:
