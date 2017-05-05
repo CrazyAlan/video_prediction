b6ebda548714be8c1b8ed6e53dfcaa089e41453d
CrazyAlan <xiaochuan.chen91@gmail.com>
2017-05-05 14:25:26 -0700

    Added kl loss

--- vae_train.py
+++ vae_train.py
@@ -289,12 +289,12 @@ def main(unused_argv):
         TRAIN_DIS = True
 
       if itr % FLAGS.print_interval == 0:
-        log_str = ('In {itr}, T_Loss {loss}, Re_Loss {recon_loss}, '\
+        log_str = ('In {itr}, T_Loss {loss}, Re_Loss {recon_loss}, Kl_loss {kl_loss}'\
                     'Dis_Loss {disc_loss}, Feat {feat_loss} '\
                     'dis_lo_ration {discr_loss_ratio}, '\
                     'LRate {learning_rate},  '\
                     'Disc_Re_acc {disc_real_acc}, pred_acc {disc_pred_acc}, '\
-                    'Enc: {TRAIN_ENC}, Gen: {TRAIN_GEN}, Dis: {TRAIN_DIS}').format(itr=itr, loss=str(loss), recon_loss=str(recon_loss),\
+                    'Enc: {TRAIN_ENC}, Gen: {TRAIN_GEN}, Dis: {TRAIN_DIS}').format(itr=itr, kl_loss=str(kl_loss), loss=str(loss), recon_loss=str(recon_loss),\
                                                                                    learning_rate=str(learning_rate), disc_real_acc=str(disc_real_acc), disc_pred_acc=str(disc_pred_acc),\
                                                                                    discr_loss_ratio=str(discr_loss_ratio),\
                                                                                    TRAIN_ENC=TRAIN_ENC, TRAIN_GEN=TRAIN_GEN, TRAIN_DIS=TRAIN_DIS,\
